652cb0730e8c2bab19d7173efce061c5bd77f418
shenglei <shenglei@csrd.cn>
2023-06-06 17:50:46 +0800

    [新增]初始化项目

---
+++ src/main/java/com/demo/snaker/controller/SnakerFlowController.java
@@ -0,0 +1,15 @@
+package com.demo.snaker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shenglei
+ * @Description 流程引擎控制器
+ * @date 2023/6/6 17:32
+ */
+@RestController
+@RequestMapping("/snaker")
+public class SnakerFlowController {
+
+}
---
+++ src/test/java/com/demo/snaker/SnakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.snaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
